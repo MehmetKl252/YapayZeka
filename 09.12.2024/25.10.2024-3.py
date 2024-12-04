719607db6b8fef4725bed8c306914c1b74931267
MehmetKl252 <50239667+MehmetKl252@users.noreply.github.com>
2024-12-04 22:50:29 +0300

    Add files via upload

---
+++ 09.12.2024/25.10.2024-3.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+img=cv.imread(r'C:\Users\Mehmet\projegoruntuisleme\myenv\sac.jpeg')
+
+b,g,r=cv.split(img)
+
+mavi_img=cv.merge((r,g,b))
+
+#cv.imshow('Mavi dama',mavi_img)
+#cv.imwrite(r'data/mavi_dama.jpg', mavi_img)
+#Tek kanal olarak  göster
+#cv.imshow('Satranc 3x3',img)
+cv.imshow('Blue Kanalı',b)
+cv.imshow('Green kanalı',g)
+cv.imshow('REd Kanalı',r)
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()
